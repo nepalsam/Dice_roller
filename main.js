cbb17b338ca91821ae68059fcfba4bbf7f8742fd
nepalsam <69169235+nepalsam@users.noreply.github.com>
2020-10-05 16:38:37 -0400

    Update main.js

--- main.js
+++ main.js
@@ -3,18 +3,18 @@
     let showRolls = document.querySelector("#showRoll");
     let lists = document.querySelector("#lists");
     roll.addEventListener("click", function () {
-      let diseRolls = [];
+      let diceRolls = [];
       let userInput = document.querySelector("#num").value;
       let num = 0;
       let total = 0;
       while (num < userInput) {
         let x = Math.floor(Math.random() * 6) + 1;
-        diseRolls.push(x);
+        diceRolls.push(x);
         total += diseRolls[num];
         num += 1 ;
       }
       totalResult.innerHTML = total;
-      console.log(diseRolls);
+      console.log(diceRolls);
       showRolls.addEventListener("click", function () {
         str = "<ol>";
         diseRolls.forEach(function (dise) {
@@ -28,12 +28,10 @@
         let num1 = 0;
         str = "<ol>";
         while(num1 < showRolls.length) {
-          str += "<li>" + diseRolls[num1] + "</li>";
+          str += "<li>" + diceRolls[num1] + "</li>";
           num1 += 1;
         }
-        // diseRolls.forEach(function (dise) {
-        //   str += "<li>" + dise + "</li>";
-        // });
+       
         str += "</ol>";
         lists.innerHTML = str;
       });
